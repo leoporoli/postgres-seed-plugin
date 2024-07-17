a2ce9de26b5fe91e49073f739615c420eadc3fe9
Gyanendra Mishra <anomaly.the@gmail.com>
2024-07-17 13:58:26 +0100

    first pass

---
+++ main.py
@@ -0,0 +1,28 @@
+import copy
+
+def create_flow(service_spec, deployment_spec, flow_uuid, seed_script, db_name, db_user, db_password):
+    modified_deployment_spec = copy.deepcopy(deployment_spec)
+
+    container = modified_deployment_spec['spec']['template']['spec']['containers'][0]
+
+    container['env'] = container.get('env', []) + [
+        {'name': 'POSTGRES_DB', 'value': db_name},
+        {'name': 'POSTGRES_USER', 'value': db_user},
+        {'name': 'POSTGRES_PASSWORD', 'value': db_password},
+    ]
+
+    init_container = {
+        'name': 'postgres-init',
+        'image': 'postgres:latest',
+        'command': ['bash', '-c', seed_script],
+        'env': container['env']
+    }
+    modified_deployment_spec['spec']['template']['spec']['initContainers'] = [init_container]
+
+    return {
+        "deployment_spec": modified_deployment_spec,
+        "config_map": {}
+    }
+
+def delete_flow(config_map, flow_uuid):
+    pass
