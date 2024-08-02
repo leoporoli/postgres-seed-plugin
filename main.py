619c16e14fd72c46acc4c519e945fdb5117ae083
Gyanendra Mishra <anomaly.the@gmail.com>
2024-08-02 16:23:11 +0100

    allow user to push multiline sql (#2)

--- main.py
+++ main.py
@@ -2,23 +2,29 @@ import copy
 
 def create_flow(service_spec, deployment_spec, flow_uuid, seed_script, db_name, db_user, db_password):
     modified_deployment_spec = copy.deepcopy(deployment_spec)
-
     container = modified_deployment_spec['template']['spec']['containers'][0]
-
     container['env'] = container.get('env', []) + [
         {'name': 'POSTGRES_DB', 'value': db_name},
         {'name': 'POSTGRES_USER', 'value': db_user},
         {'name': 'POSTGRES_PASSWORD', 'value': db_password},
     ]
-
+    
+    # Prepare the seed script
+    escaped_script = seed_script.replace("'", "'\\''")
+    init_script = f"""
+cat << EOF > /tmp/init.sql
+{escaped_script}
+EOF
+psql -U $POSTGRES_USER -d $POSTGRES_DB -f /tmp/init.sql
+"""
+    
     init_container = {
         'name': 'postgres-init',
         'image': 'postgres:latest',
-        'command': ['bash', '-c', seed_script],
+        'command': ['bash', '-c', init_script],
         'env': container['env']
     }
     modified_deployment_spec['template']['spec']['initContainers'] = [init_container]
-
     return {
         "deployment_spec": modified_deployment_spec,
         "config_map": {}
